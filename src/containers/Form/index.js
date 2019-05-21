53a5c874868cd9196c56966c117a3201a052a57f
RyanDBarnett <conner2468@gmail.com>
2019-05-21 16:31:17 -0600

    Create basic Form container

---
+++ src/containers/Form/index.js
@@ -0,0 +1,20 @@
+import React, { component } from 'react';
+
+class Form extends component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <form className='Form'>
+        <h3>Note Form</h3>
+      </form>
+    );  
+  }
+}
+
+export default Form;
