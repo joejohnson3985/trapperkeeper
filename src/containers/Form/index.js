fb3f3aecfe8b4bbf9bcc2ecce4214699d693fdac
RyanDBarnett <conner2468@gmail.com>
2019-05-21 17:11:09 -0600

    Import icons into Form component

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import add from '../../media/icons/add.svg';
+import box from '../../media/icons/check_box_blank.svg';
+import checkedBox from '../../media/icons/check_box.svg';
+import trash from '../../media/icons/delete.svg';
+
 
 class Form extends Component {
   constructor() {
@@ -12,6 +17,10 @@ class Form extends Component {
     return (
       <form className='Form'>
         <h3>Note Form</h3>
+        <img src={box} alt='Unchecked checkbox' />
+        <img src={checkedBox} alt='Checked checkbox' />
+        <img src={add} alt='Add Item' />
+        <img src={trash} alt='Delete Card' />
       </form>
     );  
   }
