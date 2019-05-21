98114be020a3e1612445ca40b68d8d22c23bc076
RyanDBarnett <conner2468@gmail.com>
2019-05-21 16:23:37 -0600

    Install redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extenions';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer, composeWithDevTools())
 
