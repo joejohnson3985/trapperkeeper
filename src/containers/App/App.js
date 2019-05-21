b48c0cbd83de657a8b82e62e6f3a80dfd2e8c58b
RyanDBarnett <conner2468@gmail.com>
2019-05-21 16:04:33 -0600

    Add trapper keeper header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,17 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trapper Keeper</h1>
+        <h2>Notebook</h2>
       </header>
     </div>
   );
