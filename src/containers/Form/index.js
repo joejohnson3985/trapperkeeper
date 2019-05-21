6acccbe4065ed7425d338789f21c41bee80c592f
RyanDBarnett <conner2468@gmail.com>
2019-05-21 17:16:29 -0600

    Add clear icon and import into Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import add from '../../media/icons/add.svg';
 import box from '../../media/icons/check_box_blank.svg';
 import checkedBox from '../../media/icons/check_box.svg';
+import clear from '../../media/icons/clear.svg';
 import trash from '../../media/icons/delete.svg';
+import trashOutline from '../../media/icons/delete_outline.svg';
 
 
 class Form extends Component {
@@ -20,7 +22,9 @@ class Form extends Component {
         <img src={box} alt='Unchecked checkbox' />
         <img src={checkedBox} alt='Checked checkbox' />
         <img src={add} alt='Add Item' />
+        <img src={clear} alt='Remove Item' />
         <img src={trash} alt='Delete Card' />
+        <img src={trashOutline} alt='Delete Card' />
       </form>
     );  
   }
