0075e5481197f78461d45baa0d2e5fd05eab1a4a
RyanDBarnett <conner2468@gmail.com>
2019-05-21 22:02:21 -0600

    Create FormItem and add handleChange, toggleCheckBox methods

---
+++ src/containers/FormItem/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import add from '../../media/icons/add.svg';
+import box from '../../media/icons/check_box_blank.svg';
+import checkedBox from '../../media/icons/check_box.svg';
+import clear from '../../media/icons/clear.svg';
+
+class FormItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      checked: false
+    }
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ value });
+  }
+
+  toggleCheckBox = (e) => {
+    const checked = !this.state.checked;
+    this.setState({ checked });
+  }
+
+  render() {
+    const {checked, value} = this.state;
+    const boxIcon = <img 
+      onClick={this.toggleCheckBox} 
+      className='icon check-box' 
+      role='checkbox' 
+      aria-checked={checked}
+      checked={checked} 
+      src={checked ? checkedBox : box} 
+      alt='Checkbox' 
+    />;
+    const plus = <img className='icon add' src={add} alt='Add item' />
+    const prevIcon = value.length ? boxIcon : plus;
+    return (
+      <div className='FormItem'>
+        {prevIcon}
+        <input 
+          className='item-text'
+          type='text'
+          onChange={this.handleChange}
+          placeholder='List item'
+          value={value}
+          contentEditable 
+        />
+        <img className='icon clear' src={clear} alt='Clear item' />
+      </div>
+    );  
+  }
+}
+
+export default FormItem;
