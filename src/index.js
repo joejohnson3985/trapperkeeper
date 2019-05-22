1646cc46b5d12b4c8d2e5f2ec4d25aa3dd5cab01
RyanDBarnett <conner2468@gmail.com>
2019-05-22 12:36:09 -0600

    Import applyMiddleware from redux and use it to apply thunk in composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import rootReducer from './reducers'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))
 
 const router = (
     <BrowserRouter>
