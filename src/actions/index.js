6af4a0c4eaf1dad79c3ab069f9ab4e10149b4160
RyanDBarnett <conner2468@gmail.com>
2019-05-22 12:39:38 -0600

    Create setNotes action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
+export const setNotes = (notes) => ({
+  type: 'SET_NOTES',
+  notes
+})
+
 export const createCard = (info) => ({
-    type: 'CREATE_CARD',
-    info
+  type: 'CREATE_CARD',
+  info
 })
