8494be87fa17c4f5ccb4248f191c570673757d73
RyanDBarnett <conner2468@gmail.com>
2019-05-22 12:55:09 -0600

    Add missing parenthesis at end of createStore

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const router = (
   <BrowserRouter>
