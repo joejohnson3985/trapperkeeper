1cc621d0cda0b1ef678dad2eae7d02a5f8abc2d7
RyanDBarnett <conner2468@gmail.com>
2019-05-22 13:09:52 -0600

    Import and connect fetchCards thunk and invoke it in componentDidMount

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { createCard } from '../../actions';
+import fetchCards from '../../thunks/fetchCards';
 import Header from '../../components/Header';
 import Form from '../Form';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Form />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount = () => {
+    this.props.fetchCards('http://localhost:3000/api/v1/notes');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Form />
+      </div>
+    ); 
+  }
 }
 
 const mapStateToProps = (state) => ({
@@ -19,7 +30,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  createCard: (info) => dispatch(createCard(info))
+  createCard: (info) => dispatch(createCard(info)),
+  fetchCards: (url) => dispatch(fetchCards(url))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
