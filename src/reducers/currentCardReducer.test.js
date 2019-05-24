8db72d19365abd29950546b57003fd9cf8c3d61e
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 22:31:49 -0600

    Add default state test for currentCardReducer

--- src/reducers/currentCardReducer.test.js
+++ src/reducers/currentCardReducer.test.js
@@ -4,7 +4,9 @@ import * as actions from '../actions/index';
 describe('currentCardReducer', ()=> {
 
   it('Should have default state', () => {
-
+    const outcome = {};
+    const result = currentCardReducer(undefined, {})
+    expect(result).toEqual(outcome)
   });
 
   it('Should set the state of a current card being viewed by the user', () => {
