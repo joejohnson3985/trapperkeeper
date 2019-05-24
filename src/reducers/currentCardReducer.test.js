7940971b7b083ba019c668374d9de73606190ef5
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 22:30:28 -0600

    Setup created for currentCardReducer

---
+++ src/reducers/currentCardReducer.test.js
@@ -0,0 +1,13 @@
+import { currentCardReducer } from './currentCardReducer';
+import * as actions from '../actions/index';
+
+describe('currentCardReducer', ()=> {
+
+  it('Should have default state', () => {
+
+  });
+
+  it('Should set the state of a current card being viewed by the user', () => {
+
+  });
+});
