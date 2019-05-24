51a2e96e5130ef78327658e4fec33fee5b03ca24
joejohnson3985 <38696582+joejohnson3985@users.noreply.github.com>
2019-05-23 21:24:44 -0600

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
   return(
     <div className='not-found'>
-      <img src={notFoundImg} atl='404 Error: page not found' />
+      <img src={notFoundImg} alt='404 Error: page not found' />
       <h1>The page you're looking for either can't be found or does not exist</h1>
       <Link to='/' className='return-home'>Click here to return to Trapper Keeper</Link>
     </div>
