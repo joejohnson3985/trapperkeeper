6f75be25f4465fff80a068d8cca0e57c608dc32b
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 22:34:43 -0600

    Add test for setCurrentCard in reducer

--- src/reducers/currentCardReducer.test.js
+++ src/reducers/currentCardReducer.test.js
@@ -10,6 +10,10 @@ describe('currentCardReducer', ()=> {
   });
 
   it('Should set the state of a current card being viewed by the user', () => {
-
+    const outcome = {id:1, name: 'Cleaning Schedule'};
+    const initialState = {};
+    const action = actions.setCurrentCard(outcome);
+    const result = currentCardReducer(initialState, action);
+    expect(result).toEqual(outcome)
   });
 });
