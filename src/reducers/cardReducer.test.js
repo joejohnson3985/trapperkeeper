f40d90ce5db7778ccee5081204b23972d8e00f9b
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 22:12:42 -0600

    Setup tests for cardReducer

---
+++ src/reducers/cardReducer.test.js
@@ -0,0 +1,17 @@
+import { cardReducer } from './cardReducer'
+import * as actions from '../actions/index'
+
+describe('cardReducer', () => {
+
+  it('Should have default state', () => {
+
+  });
+
+  it('Should create the property in store with an array', () => {
+
+  });
+
+  it('Should add a card to current array inside of the store', () => {
+
+  });
+})
