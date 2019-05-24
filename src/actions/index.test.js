7404a01251fce395bea005148eada4c4ac7f25ca
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 21:55:36 -0600

    Setup testing suite for actions

---
+++ src/actions/index.test.js
@@ -0,0 +1,16 @@
+import * as actions from './index';
+
+describe('Actions', () => {
+
+  it('should give back array of cards on type "SET_CARDS"', () => {
+
+  });
+
+  it('should create a card on type "CREATE_CARD"', () => {
+
+  });
+
+  it('should set the current card on type "SET_CURRENT_CARD"', () => {
+
+  });
+})
