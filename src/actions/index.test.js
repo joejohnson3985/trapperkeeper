9a6b46b746f2d12511223fc02616e6e83979cdae
Duy Vu <duyv.thanh@gmail.com>
2019-05-23 21:59:45 -0600

    Add test for setCards action

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -3,7 +3,13 @@ import * as actions from './index';
 describe('Actions', () => {
 
   it('should give back array of cards on type "SET_CARDS"', () => {
-
+    const cards = [{id:1, name:'Bob'}];
+    const expected = {
+      type: 'SET_CARDS',
+      cards
+    }
+    const result = actions.setCards(cards)
+    expect(result).toEqual(expected)
   });
 
   it('should create a card on type "CREATE_CARD"', () => {
