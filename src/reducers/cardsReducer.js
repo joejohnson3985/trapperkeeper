b6f9361bd62d40dc233933e76c50c402738e5051
RyanDBarnett <conner2468@gmail.com>
2019-05-27 22:04:12 -0600

    Create update card case in cardsReducer

--- src/reducers/cardsReducer.js
+++ src/reducers/cardsReducer.js
@@ -6,7 +6,11 @@ export const cardsReducer = (state=[], action) => {
       return [...state, action.card];
     case 'REMOVE_CARD':
       return state.filter(card => card.id !== action.id);
+    case 'UPDATE_CARD':
+      const index = state.findIndex(card => card.id == action.id);
+      state[index] = card;
+      return state;
     default:
-      return state
+      return state;
   }
 }
