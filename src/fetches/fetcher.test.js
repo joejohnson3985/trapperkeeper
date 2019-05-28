bd71bd02a3047fae6c4f4c5e08b2f0dbcac86dda
Duy Vu <duyv.thanh@gmail.com>
2019-05-28 13:42:19 -0600

    Set up test for fetcherPoster fetch

---
+++ src/fetches/fetcher.test.js
@@ -0,0 +1,12 @@
+import { fetcherPoster } from './fetcher';
+
+describe('fetcherPoster', () => {
+  let mockData;
+
+  beforeEach(() => {
+
+  })
+  it('should be called with the correct params', () => {
+
+  });
+})
