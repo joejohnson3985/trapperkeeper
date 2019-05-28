163fa452d628e13fa46b2a8e2bb7a5f39c515879
RyanDBarnett <conner2468@gmail.com>
2019-05-28 11:29:39 -0600

    Implement delete card on Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import './Card.scss'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './Card.scss';
+import deleteCard from '../../thunks/deleteCard';
+import trash from '../../media/icons/delete_outline.svg';
 
 const Card = (props) => {
   const { id, name, list } = props.card
@@ -9,9 +12,16 @@ const Card = (props) => {
     <Link className='card' to={cardRoute}>
       <h1>{name}</h1>
       <p>items: {list.length}</p>
+      <Link to='/'>
+        <img onClick={() => props.deleteCard(id)} src={trash} alt='Delete card' />
+      </Link>
     </Link>
   )
 }
 
 
-export default Card;
+const mapDispatchToProps = (dispatch) => ({
+  deleteCard: (id) => dispatch(deleteCard(id))
+});
+
+export default connect(null, mapDispatchToProps)(Card);
