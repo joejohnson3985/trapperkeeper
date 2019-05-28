bab433d4026c8ae31bf452de97bbaaf7895d859a
Duy Vu <duyv.thanh@gmail.com>
2019-05-28 16:20:59 -0600

    test happy path for fetchData
    Co-Authored-by: Ryan <conner2468@gmail.com>

--- src/thunks/fetchData.test.js
+++ src/thunks/fetchData.test.js
@@ -4,24 +4,28 @@ import fetchData from './fetchData';
 describe('fetchData', () => {
   let mockUrl;
   let mockCards;
+  let mockOptions;
   beforeEach(() => {
-    mockUrl = 'www.pie.com'
+    mockUrl = 'http://localhost:3000/api/v1/cards'
     mockCards = [{id:1, name:'beans', list:[1,2,3]}, {id:2, name:'not beans', list:[4,5,6]}]
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      json: () => {
-        Promise.resolve(mockCards)
-      }
+      json: () =>  Promise.resolve(mockCards)
     }))
-  })
-
-  it('should be called with the correct params', () => {
-    let mockOptions = {
+    mockOptions = {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(mockCards[0])
     }
+  })
+
+  it('should be called with the correct params', () => {
     fetchData(mockUrl, mockOptions)
     expect(window.fetch).toHaveBeenCalledWith(mockUrl, mockOptions)
+  });
+
+  it('should return a parsed response for happy path', async () => {
+    const result = await fetchData(mockUrl, mockOptions)
+    expect(result).toEqual(mockCards)
   });
 })
