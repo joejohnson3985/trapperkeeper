a19dca62b34e8586beccf2e926e1a1b0c9b3822f
Duy Vu <duyv.thanh@gmail.com>
2019-05-28 13:43:35 -0600

    set up mockImplementations

--- src/fetches/fetcher.test.js
+++ src/fetches/fetcher.test.js
@@ -4,7 +4,15 @@ describe('fetcherPoster', () => {
   let mockData;
 
   beforeEach(() => {
-
+    mockData = {id:1, name:'chores', list:[1,2,3]}
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok:true,
+        json: () => {
+          Promise.resolve(mockData)
+        }
+      })
+    })
   })
   it('should be called with the correct params', () => {
 
